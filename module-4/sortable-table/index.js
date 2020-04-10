05aa3966cedb05cf9de57ca8cf3c7c110207568b
IANomokonov <nomokonov.vana@yandex.ru>
2020-04-10 13:03:39 +0300

    modul 4

--- module-4/sortable-table/index.js
+++ module-4/sortable-table/index.js
@@ -14,11 +14,63 @@ export default class SortableTable {
   }
 
   render() {
-
+    const element = document.createElement('div');
+    element.innerHTML = this.getTable(this.data);
+    this.element = element.firstElementChild;
+    this.subElements = this.getSubElements(this.element);
   }
 
-  sort (field, order) {
+  getTable() {
+    return `
+      <div class="sortable-table">    
+        ${this.getTableHeader()}
+        <div data-elem="body" class="sortable-table__body">
+          ${this.getTableBody()}
+        </div>               
+      </div>`;
+  }
 
+  getTableHeader(field, order) {
+    return `
+      <div data-elem="header" class="sortable-table__header sortable-table__row">
+        ${this.getTableHeaderRows(field, order)}        
+      </div>
+    `
+  }
+
+  getTableHeaderRows(field, order) {
+    return this.headersConfig.map(item => {
+      return `
+        <div class="sortable-table__cell" data-name="${item.id}" data-sortable="${item.sortable}" data-order="${field == item.id ? order : ''}">
+          <span>${item.title}</span>
+          ${field == item.id ? '<span data-element="arrow" class="sortable-table__sort-arrow"><span class="sort-arrow"></span></span>' : ''}
+        </div>
+      `
+    }).join('');
+  }
+
+  getSubElements(parentElement) {
+    const elems = parentElement.querySelectorAll("[data-elem]");
+    const result = {};
+    for (let elem of elems) {
+      result[elem.dataset.elem] = elem;
+    }
+
+    return result;
+  }
+
+  getTableBody(data = this.data) {
+    return data.map(item => this.getTableRow(item)).join('');
+  }
+
+  getTableRow(dataItem) {
+    let dataItemHTML = this.headersConfig.map(function (item) {
+      return item.template
+        ? item.template(dataItem[item.id])
+        : `<div class="sortable-table__cell">${dataItem[item.id]}</div>`;
+    }).join("");
+
+    return `<a href="/products/${dataItem.id}" class="sortable-table__row">${dataItemHTML}</a>`;
   }
 
   remove() {
@@ -28,6 +80,30 @@ export default class SortableTable {
   destroy() {
     this.remove();
     this.subElements = {};
+  }
+
+  sort(field, order) {
+    console.log([field, order])
+    let sortData = this.sortData(field, order);
+    console.log(this.subElements)
+    this.subElements.header.innerHTML = this.getTableHeaderRows(field, order);
+    this.subElements.body.innerHTML = this.getTableBody(sortData);
+  }
+
+  sortData(field, order) {
+    const { sortType } = this.headersConfig.find(item => item.id === field);
+    let direction = order === 'asc' ? 1 : -1;
+
+    return this.data.sort(function (a, b) {
+      switch (sortType) {
+        case 'number':
+          return direction * (a[field] - b[field]);
+        case 'string':
+          return direction * a[field].localeCompare(b[field], 'default', { caseFirst: 'upper' });
+        default:
+          return direction * a[field].localeCompare(b[field], 'default', { caseFirst: 'upper' });
+      }
+    });
   }
 }
 
