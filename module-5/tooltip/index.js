4661e66803f400b8994bd80c309e91c7a608831f
Ivan Nomokonov <nomokonov.vana@gmail.com>
2020-04-14 19:03:24 +0300

    tooltip

--- module-5/tooltip/index.js
+++ module-5/tooltip/index.js
@@ -2,6 +2,9 @@ class Tooltip {
   static instance;
 
   element;
+  pointerOverListener;
+  pointerOutListener;
+  pointerMoveListener;
 
   constructor() {
     if (Tooltip.instance) {
@@ -11,20 +14,52 @@ class Tooltip {
     Tooltip.instance = this;
   }
 
-  initEventListeners () {
+  initEventListeners() {
+    this.pointerOverListener = (event) => {
+      if (event.target.dataset.tooltip != undefined) {
+        this.render(`<span>${event.target.dataset.tooltip}</span>`);
+      }
+    };
+    this.pointerOutListener = (event) => {
+      if (event.target.dataset.tooltip != undefined) {
+        this.remove();
+      }
+    };
 
+    document.addEventListener('pointerover', this.pointerOverListener);
+    document.addEventListener('pointerout', this.pointerOutListener);
   }
 
-  initialize () {
-
+  initialize() {
+    this.initEventListeners();
   }
 
-  render () {
+  render(html) {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    this.element.innerHTML = html;
+    document.body.append(this.element);
 
+    this.pointerMoveListener = (event) => {
+      var left = event.clientX + 10;
+      var top = event.clientY + 10;
+      this.element.style.left = left + 'px';
+      this.element.style.top = top + 'px';
+    };
+
+    document.addEventListener('pointermove', this.pointerMoveListener);
   }
 
-  destroy () {
+  remove() {
+    document.removeEventListener('pointermove', this.pointerMoveListener);
+    this.element.remove();
+    this.element = null;
+  }
 
+  destroy() {
+    document.removeEventListener('pointerover', this.pointerOverListener);
+    document.removeEventListener('pointerout', this.pointerOutListener);
+    this.remove();
   }
 }
 
